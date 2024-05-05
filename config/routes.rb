2feff5ea098ce4f7040a77c529167097df405240
EC2 Default User <ec2-user@ip-172-31-14-205.ap-southeast-2.compute.internal>
2024-05-05 18:56:18 +0000

    　”応用課題５終了”

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
   get 'relationships/followers'
   devise_for :users
   root to: "homes#top"
+  get "search" => "searches#search"
   get "home/about", to: "homes#about"
   delete 'books/:id' => 'books#destroy', as: 'destroy_book'
   resources :books, only: [:new,  :create, :index, :show, :edit, :update] do
