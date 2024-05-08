d90430c4e1c01af8a752197f99654a4abbc15d8f
EC2 Default User <ec2-user@ip-172-31-14-205.ap-southeast-2.compute.internal>
2024-05-08 15:45:46 +0000

    いいね合計多い順

---
+++ app/controllers/searches_controller.rb
@@ -0,0 +1,18 @@
+class SearchesController < ApplicationController
+  before_action :authenticate_user!
+
+  def index
+   render 'search' 
+  end
+  
+  def search
+    @range = params[:range]
+    @word = params[:word]
+
+    if @range == "User"
+      @users = User.looks(params[:search], params[:word])
+    else
+      @books = Book.looks(params[:search], params[:word])
+    end
+  end
+end
