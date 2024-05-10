ed67b5aab682a7d45a03c1c20ec219e3d372b82a
EC2 Default User <ec2-user@ip-172-31-14-205.ap-southeast-2.compute.internal>
2024-05-10 10:57:22 +0000

    応用課題a終わり

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,15 +6,20 @@ class User < ApplicationRecord
          
   has_one_attached :profile_image
    has_many :books, dependent: :destroy
+  
    has_many :favorites, dependent: :destroy
    has_many :book_comments, dependent: :destroy
+  
    has_many :followers, class_name: "Relationship", foreign_key: "follower_id", dependent: :destroy
    has_many :followeds, class_name: "Relationship", foreign_key: "followed_id", dependent: :destroy
    has_many :following_users, through: :followers, source: :followed
    has_many :follower_users, through: :followeds, source: :follower
+   
    has_many :entries, dependent: :destroy
    has_many :messages, dependent: :destroy
    has_many :rooms, through: :entries
+   
+   has_many :view_counts, dependent: :destroy
    validates :name, presence: true, length: { minimum: 2, maximum: 20 }, uniqueness: true
    validates :introduction,length: { maximum: 50 } 
   
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2024_05_08_155850) do
+ActiveRecord::Schema.define(version: 2024_05_10_100336) do
 
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
@@ -103,6 +103,13 @@ ActiveRecord::Schema.define(version: 2024_05_08_155850) do
     t.datetime "remember_created_at"
     t.string "name"
     t.text "introduction"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "view_counts", force: :cascade do |t|
+    t.integer "book_id"
+    t.integer "user_id"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
